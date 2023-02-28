ecdb15ac6f050283239a6661dcae45532315c2fe
Aashish Bansal <aashish22bansal@gmail.com>
2023-02-28 18:07:01 +0530

    Project Completed

---
+++ Flying Eagle/Flying Eagle/Flying_EagleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flying_EagleApp.swift
+//  Flying Eagle
+//
+//  Created by Aashish Bansal on 27/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Flying_EagleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Flying Eagle/Flying EagleUITests/Flying_EagleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Flying_EagleUITestsLaunchTests.swift
+//  Flying EagleUITests
+//
+//  Created by Aashish Bansal on 27/02/23.
+//
+
+import XCTest
+
+final class Flying_EagleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
